355616b9720f453255610a02681ec22917421b29
Yash Shah <shah.yash183@gmail.com>
2020-04-26 21:59:09 -0400

    Create 2 separate files for single and multi label tracking

--- multi_label_tracking.py
+++ multi_label_tracking.py
@@ -1,16 +1,15 @@
+#track multiple objects and not meant to run 
 import numpy as np
 import argparse
 import cv2 
 import time
 from imutils.video import FPS 
 
-
-
 # construct the argument parse 
 parser = argparse.ArgumentParser(description='Script to run Object trackers using opencv')
 parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
-parser.add_argument("--thr", default=0.9, type=float, help="confidence threshold to filter out weak detections")
-parser.add_argument("--frame_count", default='2',help="run the object detector every n frames")
+parser.add_argument("--thr", default=0.6, type=float, help="confidence threshold to filter out weak detections")
+parser.add_argument("--frame_count", default='5',help="run the object detector every n frames")
 parser.add_argument("--output",default = False,help = "create output video file")
 args = parser.parse_args()
 
@@ -33,8 +32,7 @@ else:
 net = cv2.dnn.readNetFromCaffe("mobilenet/MobileNetSSD_deploy.prototxt", "mobilenet/MobileNetSSD_deploy.caffemodel")
 fps = FPS().start()
 total_frames = 1
-_, prev_fram = cap.read()
-prev_frame = cv2.cvtColor(prev_fram, cv2.COLOR_BGR2GRAY)
+_, prev_frame = cap.read()
 tracker_count = 0
 tracking_started = False
 
@@ -47,21 +45,19 @@ while True:
     if frame is None: #end of video file
         print(tracker_count)
         break
-    curr_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_resized = cv2.resize(frame,(300,300)) # reshaping frame to (1, 3, 300, 300)
-    # check for motion, if not restart the loop#then detect the object
+    frame_resized = cv2.resize(frame,(300,300)) # reshaping frame to (300,300)
+    # running the object detector every nth frame 
     if total_frames % int(args.frame_count)-1 == 0:
-        #print('Motion detected')
+
         blob = cv2.dnn.blobFromImage(frame_resized, 0.007843, 
             (frame_resized.shape[1],frame_resized.shape[0]), (127.5, 127.5, 127.5), crop = False)
         net.setInput(blob)
         detections = net.forward()
-
-        centroids = np.zeros([1, 1, 2], dtype=np.float32)
         # object to be tracked's probability should be greater than the threshold
         idx = np.argwhere(detections[0, 0, :, 2] >= args.thr)
         num_detections = len(idx)
         num_centroids = 0
+        centroids = np.zeros([1, 1, 2], dtype=np.float32)
         for i in range(0,len(idx)):     
 
             tracking_id = int(detections[0, 0, idx[i], 1]) 
@@ -83,39 +79,31 @@ while True:
             xRightTop   = int(widthFactor * xRightTop)
             yRightTop   = int(heightFactor * yRightTop)
 
-            # Draw label and confidence of prediction in frame resized
-            label = labels[tracking_id] +": "+ str(confidence)+ ": " + str(total_frames)              
-            print(label) # print class and confidence
+            # print class and confidence
+            label = labels[tracking_id] +": "+ str(confidence)             
+            print(label) 
 
             x = (xLeftBottom + xRightTop)/2
             y = (yLeftBottom + yRightTop)/2
-            centroid = np.array([[[x,y]]],dtype=np.float32)
-            if i >= 2 : # 2 or more detections
-                dist = np.linalg.norm(centroid.squeeze() - centroids.squeeze(),
-                                axis = centroids.squeeze().ndim -1)
-                if isinstance(dist,np.float32):
-                    min_dist = dist
-                else :
-                    min_dist = dist[np.argmin(dist)]
-                if 20 < min_dist  and num_centroids <= num_detections: # min dist threshold to consider motion
-                    frame = cv2.circle(frame, (int(x),int(y)), 15, (0,0,255), -1)
-                    #print("Hi")
-                    num_centroids += 1
-                    tracking_started = True
-                    tracker_count += 1
-                    centroids = np.append(centroids,centroid,axis = 0)
 
+            # draw the centroid on the frame
+            frame = cv2.circle(frame, (int(x),int(y)), 15, (0,0,255), -1)
+            num_centroids += 1
+            tracking_started = True
+            tracker_count += 1
+            if i == 0:
+                centroids[0,0,0] = x
+                centroids[0,0,1] = y
             else:
+                centroid = np.array([[[x,y]]],dtype=np.float32)
                 centroids = np.append(centroids,centroid,axis = 0)
-                frame = cv2.circle(frame, (int(x),int(y)), 15, (0,0,255), -1)
-                num_centroids += 1
 
 
 
 
-    else:
-        if len(centroids) and tracking_started and num_detections:
-            next1, st, error = cv2.calcOpticalFlowPyrLK(prev_frame, curr_frame,
+    else:   # track an object only if it has been detected
+        if centroids.sum() != 0 and tracking_started and num_detections:
+            next1, st, error = cv2.calcOpticalFlowPyrLK(prev_frame, frame,
                                             centroids, None, **lk_params)
 
             good_new = next1[st==1]
@@ -126,22 +114,23 @@ while True:
                 a, b = new.ravel()
                 c, d = old.ravel()
                 distance = np.sqrt((a-c)**2 + (b-d)**2)
-                if 20 < distance < 200 and num_centroids <= num_detections: # min dist threshold
+                # distance between new and old points should fall within
+                # specific values for 2 points to be same the object
+                if 20 < distance < 200 and num_centroids <= num_detections:
                     frame = cv2.circle(frame, (a, b), 15, (0,0,255), -1)
                     num_centroids += 1
 
             centroids = good_new.reshape(-1, 1, 2)
 
 
-
     total_frames += 1
     fps.update()
     fps.stop()
-    # print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
     print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
     if args.output:
         writer.write(frame)
     cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
     cv2.imshow("frame", frame)
+    prev_frame = frame
     if cv2.waitKey(1) >= 0:  # Break with ESC 
         break
